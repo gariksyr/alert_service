6a354778796157afed4ed788b230e0fd25c4fce1
Игорь <gariksyr@gmail.com>
2026-04-10 20:35:07 +0300

    unused imports

--- src/main/java/com/thesis/alert_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thesis/alert_service/exception/GlobalExceptionHandler.java
@@ -5,13 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
 
 @Setter
 @Getter
