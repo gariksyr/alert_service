10ab28d3208e03d256536fbd0b508fd52cf1c323
Игорь <gariksyr@gmail.com>
2026-04-29 18:30:28 +0300

    service layer and transaction

--- src/main/java/com/thesis/alert_service/kafka/AlertConsumer.java
+++ src/main/java/com/thesis/alert_service/kafka/AlertConsumer.java
@@ -3,12 +3,13 @@ package com.thesis.alert_service.kafka;
 import com.thesis.alert_service.dto.MeasurementEvent;
 import com.thesis.alert_service.model.Alert;
 import com.thesis.alert_service.model.Zone;
-import com.thesis.alert_service.repository.AlertRepository;
-import com.thesis.alert_service.repository.ZoneRepository;
+import com.thesis.alert_service.service.AlertService;
+import com.thesis.alert_service.service.ZoneService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,11 +19,12 @@ import java.util.List;
 @Slf4j
 public class AlertConsumer {
 
-    private final AlertRepository alertRepository;
-    private final ZoneRepository zoneRepository;
+    private final AlertService alertService;
+    private final ZoneService zoneService;
     private static final double SPEED_LIMIT = 25.0;
 
     @KafkaListener(topics = "telemetry-topic", groupId = "alert-group")
+    @Transactional
     public void consume(MeasurementEvent event) {
         log.info("Received telemetry for vessel: {}", event.getVesselImo());
 
@@ -33,10 +35,10 @@ public class AlertConsumer {
             alert.setMessage("Vessel exceeded speed limit: " + event.getSpeed() + " knots");
             alert.setTimestamp(LocalDateTime.now());
 
-            alertRepository.save(alert);
+            alertService.saveAlert(alert);
             log.warn("ALERT CREATED: Vessel {} is going too fast!", event.getVesselImo());
         }
-        List<Zone> zones = zoneRepository.findZonesContainingPoint(event.getLatitude(), event.getLongitude());
+        List<Zone> zones = zoneService.findZonesContainingPoint(event.getLatitude(), event.getLongitude());
 
         for (Zone zone : zones) {
             Alert alert = new Alert();
@@ -44,7 +46,7 @@ public class AlertConsumer {
             alert.setAlertType("ZONE_VIOLATION");
             alert.setMessage("Vessel come to forbidden zone: " + zone.getName());
             alert.setTimestamp(LocalDateTime.now());
-            alertRepository.save(alert);
+            alertService.saveAlert(alert);
             log.warn("WARNING: Vessel {} in the zone {}!", event.getVesselImo(), zone.getName());
     }
 }
